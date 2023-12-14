644c82336be0741cb6bee3a7c513023e16314523
zamaniex <ext-ali.zamani@chu-lyon.fr>
2023-12-14 13:45:26 +0100

    3 - decorateur ajout version HTS

--- 3_Decorateur/Program.cs
+++ 3_Decorateur/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _3_Decorateur;
+using _3_Decorateur.VersionHTS;
 
 
 //readme => ne fonctionne pas comme attendu
@@ -16,3 +17,17 @@ b2 = new IngredientChocolat(b2);
 b2 = new IngredientChocolat(b2);
 b2 = new IngredientCaramel(b2);
 Console.WriteLine(b2.GetDescription() + ", " + b2.Cout() + "euros");
+
+
+
+
+
+//test version HTS
+AzureNewsLetter azure = new AzureNewsLetter();
+//await azure.SendNewsLetter("envoie depuis azure");
+DatabaseNewsLetterDecorateur dbsender = new DatabaseNewsLetterDecorateur(azure);
+StatistiqueNewsLetterDecorateur statSender = new StatistiqueNewsLetterDecorateur(dbsender);
+
+await statSender.SendNewsLetter("1er courrier");
+await statSender.SendNewsLetter("2e courrier");
+Console.WriteLine(StatistiqueNewsLetterDecorateur.NbletterEnvoye);
---
+++ 3_Decorateur/VersionHTS/DecorateurHts.cs
@@ -0,0 +1,90 @@
+﻿namespace _3_Decorateur.VersionHTS
+{
+    //décorateur de données
+    public class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+    }
+
+    public class PersonWrapper
+    {
+        private Personne personne;
+        public string Nom
+        {
+            get => personne.Nom;
+            set => personne.Nom = value;
+        }
+
+        public DateTime DateInscription { get; set; }
+        public string NomComplet => $"{personne.Prenom} {personne.Nom}";
+
+        public PersonWrapper(Personne p)
+        {
+            personne = p;
+        }
+    }
+
+
+
+
+    //décorateur comportemental
+    public interface INewsLetterSender
+    {
+        Task<bool> SendNewsLetter(string contenu);
+    }
+
+    public class AzureNewsLetter : INewsLetterSender
+    {
+        public Task<bool> SendNewsLetter(string contenu)
+        {
+            Console.WriteLine("Newsletter envoyé depuis Azure");
+            return Task.FromResult(true);
+        }
+    }
+
+    public abstract class NewsLetterDecorateur : INewsLetterSender
+    {
+        private readonly INewsLetterSender _newsLetter;
+        public NewsLetterDecorateur(INewsLetterSender newsletter) {
+            _newsLetter = newsletter;
+        }
+
+        public virtual Task<bool> SendNewsLetter(string contenu)
+        {
+            return _newsLetter.SendNewsLetter(contenu);
+        }
+    }
+
+    public class DatabaseNewsLetterDecorateur : NewsLetterDecorateur
+    {
+        public DatabaseNewsLetterDecorateur(INewsLetterSender newsletter) : base(newsletter)
+        {
+
+        }
+
+        public override Task<bool> SendNewsLetter(string contenu)
+        {
+            Console.WriteLine("Newsletter sauvegardé en base" + contenu);
+            return base.SendNewsLetter(contenu);
+        }
+    }
+
+    public class StatistiqueNewsLetterDecorateur : NewsLetterDecorateur
+    {
+        public static int NbletterEnvoye = 0;
+        public StatistiqueNewsLetterDecorateur(INewsLetterSender newsletter) : base(newsletter)
+        {
+
+        }
+
+        public override Task<bool> SendNewsLetter(string contenu)
+        {
+            NbletterEnvoye++;
+            Console.WriteLine("send depuis statistique " + contenu);
+            return base.SendNewsLetter(contenu);
+        }
+
+    }
+
+}
