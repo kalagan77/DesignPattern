52c8f6c83683d4e87316342f23607ba9a295b1e7
kalagan77 <azamani@live.fr>
2023-12-19 21:48:20 +0100

    4 - factory pizza V0.1

---
+++ 4_Factory/Pizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Factory
+{
+    public class Pizza
+    {
+        public void Preparer()
+        {
+            throw new NotImplementedException();
+        }
+        public void Cuire()
+        {
+            throw new NotImplementedException();
+        }
+        public void Couper()
+        {
+            throw new NotImplementedException();
+        }
+        public void Emballer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 4_Factory/Pizzeria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Factory
+{
+    public abstract class Pizzeria
+    {
+        public Pizza CommanderPizza(string type)
+        {
+            Pizza pizza;
+            pizza = CreerPizza(type);
+
+            pizza.Preparer();
+            pizza.Cuire();
+            pizza.Couper();
+            pizza.Emballer();
+
+            return pizza;
+        }
+
+        public abstract Pizza CreerPizza(string type);
+    }
+}
