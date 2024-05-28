521c2fbe41102071b8d4dad259edd2612c1d14d7
zamaniex <ext-ali.zamani@chu-lyon.fr>
2024-05-28 13:31:43 +0200

    Etat V0.1

---
+++ 9_Etat/Distributeur.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Etat
+{
+    internal class Distributeur
+    {
+        private TypeEtat etat = TypeEtat.EPUISE;
+        private int _nombre = 0;
+
+        public Distributeur(int nombre)
+        {
+            _nombre = nombre;
+            if(nombre > 0)
+            {
+                etat = TypeEtat.SANS_PIECE;
+            }
+                
+        }
+
+    }
+}
---
+++ 9_Etat/TypeEtat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Etat
+{
+    public enum TypeEtat
+    {
+        EPUISE = 0,     //pas de pièce
+        SANS_PIECE= 1,
+        A_PIECE = 2,
+        VENDU = 3
+    }
+}
