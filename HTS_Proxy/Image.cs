78dac8d4b8704415706f6aff144e3eeb13b106c8
zamaniex <ext-ali.zamani@chu-lyon.fr>
2024-06-13 13:27:18 +0200

    Hts proxy

---
+++ HTS_Proxy/Image.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTS_Proxy
+{
+    public interface IImage
+    {
+        void DisplayImage();
+    }
+
+    public class Image : IImage
+    {
+        private readonly string _filename;
+        private byte[] data;
+
+        public Image(string filename)
+        {
+            _filename = filename;
+            Thread.Sleep(1000);
+        }
+
+        public void DisplayImage()
+        {
+            Console.WriteLine("Affichage de l'image - Implémentation");
+        }
+    }
+
+    public class ImageProxy : IImage
+    {
+        private readonly string _filename;
+        public ImageProxy(string filename)
+        {
+            _filename = filename;
+        }
+        public void DisplayImage()
+        {
+            Image image = new Image(this._filename);
+            Console.WriteLine("Affichage de l'image - Implémentation");
+        }
+    }
+
+
+}
---
+++ HTS_Proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using HTS_Proxy;
+
+Console.WriteLine("début : "+ DateTime.UtcNow.ToString("HH:mm:ss:fff"));
+Image image = new Image(@"c:\test\image.jpg");
+//image.DisplayImage();
+Console.WriteLine("fin : " + DateTime.UtcNow.ToString("HH:mm:ss:fff"));
+
+
+Console.WriteLine("début : " + DateTime.UtcNow.ToString("HH:mm:ss:fff"));
+ImageProxy image2 = new ImageProxy(@"c:\test\image.jpg");
+Console.WriteLine("fin : " + DateTime.UtcNow.ToString("HH:mm:ss:fff"));
+image.DisplayImage();
+
+
+Console.WriteLine("début : " + DateTime.UtcNow.ToString("HH:mm:ss:fff"));
+//autres méthodes fournies par le framework, en utilisant le lazy
+Lazy<Image> image3 = new Lazy<Image>(() => new Image(@"c:\test\image.jpg"));
+Console.WriteLine("fin : " + DateTime.UtcNow.ToString("HH:mm:ss:fff"));
+image3.Value.DisplayImage();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
